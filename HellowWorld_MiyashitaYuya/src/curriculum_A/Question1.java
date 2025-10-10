ab2e0f0802661199c49da0a66a2175027aeea37d
yuy64 <yuy64@miyamoto>
2025-10-10 21:31:51 +0900

    課題2-10 再提出

--- HellowWorld_MiyashitaYuya/src/curriculum_A/Question1.java
+++ HellowWorld_MiyashitaYuya/src/curriculum_A/Question1.java
@@ -4,7 +4,8 @@ public class Question1 {
 	
 	public static void main(String[] args) {
 
-		// Q1 &Q2
+		// Q1 各型の変数を宣言し、初期値を設定してください。
+		// Q2 1で宣言した各型の変数に指定された値を代入してください
 		byte byteNum = 10;
 		short shortNum = 100;
 		int intNum = 1000;
@@ -16,7 +17,7 @@ public class Question1 {
 		boolean isBoolean = true;
 		
 		
-		// Q3
+		// Q3 値を代入した変数を用いて出力をしてください
 		System.out.println(byteNum + shortNum + intNum + longNum); // 11110を出力
 		System.out.println(byteNum + byteNum); // 20を出力
 		System.out.println(letter + " " + letters + " " + isBoolean); // a ハロー trueを出力 
@@ -27,71 +28,71 @@ public class Question1 {
 		System.out.println(); // 空白行
 		
 		
-		// Q4
-		String name = "山田太郎";
-		System.out.println("こんにちは、" + name + "さん！");
+		// Q4 nameというString型の変数を宣言する。
+		String name = "山田太郎"; // name変数に"山田太郎"を代入
+		System.out.println("こんにちは、" + name + "さん！"); // コンソールに こんにちは、山田太郎さん！を出力
 		System.out.println(); // 空白行
 		
 		
-		// Q5
-		int age = 25;
-		System.out.println("年齢:" + age + "歳");
+		// Q5 ageというint型の変数を宣言する。
+		int age = 25; // ageに25を代入
+		System.out.println("年齢:" + age + "歳"); // コンソールに 年齢:25歳 を出力
 		System.out.println(); // 空白行
 		
 		
-		// Q6
-		int num1 = 10;
-		int num2 = 5;
-		int sum = (num1 + num2);
-		System.out.println(sum);
+		// Q6 num1とnum2というint型の変数を宣言する。
+		int num1 = 10; // num1に10を代入
+		int num2 = 5; // num2nに5を代入
+		int sum = (num1 + num2); // num1とnum2を足した結果をsumというint型の変数に代入
+		System.out.println(sum); // コンソールにsumの結果を出力
 		System.out.println(); // 空白行
 		
 		
-		// Q7
-		int score = 80;
-		System.out.println("最終スコア:" + (score + 20));
+		// Q7 scoreというint型の変数を宣言
+		int score = 80; // scoreに80を代入
+		System.out.println("最終スコア:" + (score + 20)); // scoreに20を加え、コンソールに 最終スコア:100 を出力
 		System.out.println(); // 空白行
 		
 		
-		//Q8
-		double price = 99.99;
-		System.out.println("整数価格:" + (int)price);
+		//Q8 priceというdouble型の変数を宣言
+		double price = 99.99; // priceに99.99を代入
+		System.out.println("整数価格:" + (int)price); // priceをint型に変換し、コンソールに 整数価格:99と出力 
 		System.out.println(); // 空白行
 		
 		
-		//Q9
-		String numStr = "123";
-		int numStr2 = Integer.parseInt(numStr);
-		System.out.println("変換後の値" + (numStr2 + 10));
+		//Q9 String型の変数numStrを宣言
+		String numStr = "123"; // numStrに"123"を代入
+		int numStr2 = Integer.parseInt(numStr); // numStrをint型に変換
+		System.out.println("変換後の値" + (numStr2 + 10)); // numStrに10を足した結果をコンソールに出力
 		System.out.println(); // 空白行
 		
 		
-		// Q10
-		int num = 50;
-		String numS = String.valueOf(num);
-		System.out.println("得点:" + (numS) + "点");
+		// Q10 int型の変数numを宣言
+		int num = 50; // numに50を代入
+		String numS = String.valueOf(num); // numをString型に変換
+		System.out.println("得点:" + (numS) + "点"); // コンソールに 得点:50点と出力
 		System.out.println(); // 空白行
 		
 		
-		// Q11
-		int a = 10;
-		int b = 20;
-		boolean result = (a < b);
-		System.out.println(result);
+		// Q11 aがbより小さいかどうかを条件演算子を使用して作成
+		int a = 10; // int型変数aに10を代入
+		int b = 20; // int型変数bに20を代入
+		boolean result = (a < b); // aがbより小さいかboolean変数resultに代入
+		System.out.println(result); // コンソールにresultの結果を出力
 		System.out.println(); // 空白行	
 		
 		
-		// Q12
-		int x = 15;
-		String message = (x > 10) ? "OK" : "NG";
-		System.out.println(message);
+		// Q12 条件演算子(三項演算子)を使用すること
+		int x = 15; // int型変数xに15を代入
+		String message = (x > 10) ? "OK" : "NG"; // String型変数messageにxが10以上なら"OK"そうでなければ"NG"を代入
+		System.out.println(message); // messageの結果をコンソールに出力
 		System.out.println(); // 空白行	
 		
 		
-		//Q13
-		String Java = "Python";
-		String text = "私は" + Java + "が大好きです。" + Java + "は楽しい！";
-		System.out.println(text);
+		// Q13 String text = "私はJavaが好きです。Javaは楽しい！";という文章の「Java」を「Python」に変換する。
+		String java = "Python"; // String型変数javaにPythonを代入
+		String text = "私は" + java + "が好きです。" + java + "は楽しい！"; // String型変数textの文章にjavaを代入
+		System.out.println(text); // コンソールにtextの結果を出力
 		
 			
 	}	
